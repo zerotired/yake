f444b60fad5fbd1b453311e2cb83fd51fcfd4ad7
Tim Eggert <tim.eggert@celeraone.com>
2019-01-14 23:39:07 +0100

    First shot for recursive create

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,12 @@ extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_yaml;
+extern crate walkdir;
 
 use args::create_cli_app;
 use std::process::exit;
-use yaml::load_yml_from_file;
+use yaml::{load_yml_from_file, load_yml_from_subdirs};
+use std::env;
 
 mod args;
 pub mod yake;
@@ -20,6 +22,14 @@ fn main() {
     let yake_args = create_cli_app();
 
     let yake = load_yml_from_file("Yakefile");
+
+    let path= env::current_dir().unwrap();
+
+    let sub_yakes = match yake.meta.include_recursively {
+        Some(true) => load_yml_from_subdirs(path.to_str().unwrap()).unwrap(),
+        _ => Vec::new()
+    };
+
     match yake.has_target_name(&yake_args.target) {
         Err(x) => {
             eprintln!(
